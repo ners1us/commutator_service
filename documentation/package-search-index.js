a942652567bdcc9c3314d3f813c52df372aa40e9
ners1us <66124861+ners1us@users.noreply.github.com>
2024-08-10 15:06:01 +0300

    chore: delete redundant files

--- documentation/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Entities.CdrGenerator"},{"l":"Entities.SystemManager"},{"l":"Entities.UdrGenerator"},{"l":"Models"},{"l":"Services"}];updateSearchResults();
